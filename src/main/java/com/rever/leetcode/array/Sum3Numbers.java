93dbb9a995ed6a2b175ec837c26dbd636ea6db43
haiyang <haiyang.he@bigdatacd.com>
2018-12-25 11:06:00 +0800

    找出数组中和等于0的三个数，并且这三个数不能重复，添加了复杂度为O(n2)的实现

--- src/main/java/com/rever/leetcode/array/Sum3Numbers.java
+++ src/main/java/com/rever/leetcode/array/Sum3Numbers.java
@@ -32,4 +32,47 @@ public class Sum3Numbers {
 
         return result;
     }
+
+    public List<int[]> threeSumEqual0Simple(int[] array) {
+        if (array.length < 3) {
+            return Collections.emptyList();
+        }
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < array.length - 2; i++) {
+            int j = i + 1;
+            int k = array.length - 1;
+            int[] subArray = new int[3];
+            while (j < k) {
+                int sum = array[i] + array[j] + array[k];
+                if(sum == 0){
+                    subArray[0] = array[i];
+                    subArray[1] = array[j];
+                    subArray[2] = array[k];
+
+                    result.add(subArray);
+                    ++j;
+                    --k;
+
+                    //因为数组里有重复的元素，
+                    while (j < k && array[j - 1] == array[j]){
+                        ++j;
+                    }
+
+                    while (j < k && array[k] == array[k + 1]){
+                        --k;
+                    }
+                } else if (sum < 0) {
+                    //因为排过序，所以当和小于0的时候，就要往大里取数
+                    ++j;
+                } else {
+                    //因为排过序，所以当和大于0的时候，就要往小里取数
+                    --k;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/com/rever/leetcode/array/Sum3NumbersTest.java
+++ src/test/java/com/rever/leetcode/array/Sum3NumbersTest.java
@@ -34,5 +34,27 @@ public class Sum3NumbersTest {
 
     }
 
+    @Test
+    public void threeSumEqual0Simple_should_return_emtpy_when_the_given_array_less_than_3_element() {
+        int[] array = new int[]{1, 2};
+
+        List<int[]> result = sum3Numbers.threeSumEqual0Simple(array);
+
+        assertThat(result.isEmpty(), is(true));
+    }
+
+    @Test
+    public void threeSumEqual0Simple_should_return_sorted_three_elements_the_sum_of_them_equal_to_0_when_the_given_array() {
+        int[] array = new int[]{-2, -1, 3};
+
+        List<int[]> result = sum3Numbers.threeSumEqual0Simple(array);
+
+        assertThat(result.isEmpty(), is(false));
+        int[] elements = result.get(0);
+
+        assertThat(elements, equalTo(new int[]{-2, -1, 3}));
+
+    }
+
 
 }
