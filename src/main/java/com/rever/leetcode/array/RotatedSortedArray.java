d12675944ef1fa82aa1353fb63ae457e6dd64290
haiyang <haiyang.he@bigdatacd.com>
2018-12-28 09:50:10 +0800

    轮转的排序数组里找最小的值，添加了非递归实现

--- src/main/java/com/rever/leetcode/array/RotatedSortedArray.java
+++ src/main/java/com/rever/leetcode/array/RotatedSortedArray.java
@@ -1,7 +1,7 @@
 package com.rever.leetcode.array;
 
 public class RotatedSortedArray {
-    public int findMin(int[] array) {
+    public int findMinLoop(int[] array) {
         if (array.length == 0) {
             return 0;
         }
@@ -11,10 +11,41 @@ public class RotatedSortedArray {
         if (array.length == 2) {
             return Math.min(array[0], array[1]);
         }
-        return findMin(array, 0, array.length - 1);
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end - 1) {
+            if (array[start] < array[end]) {
+                return array[start];
+            }
+
+            int mid = start + (end - start) / 2;
+
+            if (array[start] < array[mid]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        return Math.min(array[start], array[end]);
     }
 
-    private int findMin(int[] array, int start, int end) {
+    public int findMinRecursion(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        if (array.length == 1) {
+            return array[0];
+        }
+        if (array.length == 2) {
+            return Math.min(array[0], array[1]);
+        }
+        return findMinRecursion(array, 0, array.length - 1);
+    }
+
+    private int findMinRecursion(int[] array, int start, int end) {
         if (array[start] < array[end]) {
             return array[start];
         }
@@ -26,9 +57,9 @@ public class RotatedSortedArray {
         int midIndex = start + (end - start) / 2;
 
         if (array[start] < array[midIndex]) {
-            return findMin(array, midIndex, end);
+            return findMinRecursion(array, midIndex, end);
         } else {
-            return findMin(array, start, midIndex);
+            return findMinRecursion(array, start, midIndex);
         }
     }
 }
--- src/test/java/com/rever/leetcode/array/RotatedSortedArrayTest.java
+++ src/test/java/com/rever/leetcode/array/RotatedSortedArrayTest.java
@@ -11,14 +11,14 @@ public class RotatedSortedArrayTest {
 
     @Test
     public void findMin_should_return_0_when_given_array_is_empty() {
-        int min = rotatedSortedArray.findMin(new int[]{});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{});
 
         assertThat(min, is(0));
     }
 
     @Test
     public void findMin_should_return_1_when_given_array_is_1() {
-        int min = rotatedSortedArray.findMin(new int[]{1});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{1});
 
         assertThat(min, is(1));
     }
@@ -26,14 +26,14 @@ public class RotatedSortedArrayTest {
 
     @Test
     public void findMin_should_return_1_when_given_array_is_1_2() {
-        int min = rotatedSortedArray.findMin(new int[]{1, 2});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{1, 2});
 
         assertThat(min, is(1));
     }
 
     @Test
     public void findMin_should_return_1_when_given_array_is_2_1() {
-        int min = rotatedSortedArray.findMin(new int[]{2, 1});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{2, 1});
 
         assertThat(min, is(1));
     }
@@ -41,7 +41,7 @@ public class RotatedSortedArrayTest {
 
     @Test
     public void findMin_0_when_given_array_is_0_1_2_3_4_5_6_7() {
-        int min = rotatedSortedArray.findMin(new int[]{0, 1, 2, 3, 4, 5, 6, 7});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{0, 1, 2, 3, 4, 5, 6, 7});
 
         assertThat(min, is(0));
     }
@@ -49,21 +49,79 @@ public class RotatedSortedArrayTest {
 
     @Test
     public void findMin_0_when_given_array_is_4_5_6_7_0_1_2_3() {
-        int min = rotatedSortedArray.findMin(new int[]{4, 5, 6, 7, 0, 1, 2, 3});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{4, 5, 6, 7, 0, 1, 2, 3});
 
         assertThat(min, is(0));
     }
 
     @Test
     public void findMin_0_when_given_array_is_7_0_1_2_3_4_5_6() {
-        int min = rotatedSortedArray.findMin(new int[]{7, 0, 1, 2, 3,4, 5, 6});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{7, 0, 1, 2, 3,4, 5, 6});
 
         assertThat(min, is(0));
     }
 
     @Test
     public void findMin_0_when_given_array_is_5_6_7_0_1_2_3() {
-        int min = rotatedSortedArray.findMin(new int[]{ 5, 6, 7, 0, 1, 2, 3});
+        int min = rotatedSortedArray.findMinRecursion(new int[]{ 5, 6, 7, 0, 1, 2, 3});
+
+        assertThat(min, is(0));
+    }
+    @Test
+    public void findMinLoop_should_return_0_when_given_array_is_empty() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{});
+
+        assertThat(min, is(0));
+    }
+
+    @Test
+    public void findMinLoop_should_return_1_when_given_array_is_1() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{1});
+
+        assertThat(min, is(1));
+    }
+
+
+    @Test
+    public void findMinLoop_should_return_1_when_given_array_is_1_2() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{1, 2});
+
+        assertThat(min, is(1));
+    }
+
+    @Test
+    public void findMinLoop_should_return_1_when_given_array_is_2_1() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{2, 1});
+
+        assertThat(min, is(1));
+    }
+
+
+    @Test
+    public void findMinLoop_0_when_given_array_is_0_1_2_3_4_5_6_7() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{0, 1, 2, 3, 4, 5, 6, 7});
+
+        assertThat(min, is(0));
+    }
+
+
+    @Test
+    public void findMinLoop_0_when_given_array_is_4_5_6_7_0_1_2_3() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{4, 5, 6, 7, 0, 1, 2, 3});
+
+        assertThat(min, is(0));
+    }
+
+    @Test
+    public void findMinLoop_0_when_given_array_is_7_0_1_2_3_4_5_6() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{7, 0, 1, 2, 3,4, 5, 6});
+
+        assertThat(min, is(0));
+    }
+
+    @Test
+    public void findMinLoop_0_when_given_array_is_5_6_7_0_1_2_3() {
+        int min = rotatedSortedArray.findMinLoop(new int[]{ 5, 6, 7, 0, 1, 2, 3});
 
         assertThat(min, is(0));
     }
