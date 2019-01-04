b28029829669d4fa1efb64e074466e18227e9eec
haiyang <haiyang.he@bigdatacd.com>
2019-01-04 10:24:27 +0800

    重构计算最大的矩形区域

--- src/main/java/com/rever/leetcode/array/Rectangle.java
+++ src/main/java/com/rever/leetcode/array/Rectangle.java
@@ -15,15 +15,19 @@ public class Rectangle {
         int i = 0;
 
         while (i < height.size()) {
-            if (s.isEmpty() || height.get(i) > height.get(s.get(s.size() - 1))) {
+            if (s.isEmpty() || height.get(i) > height.get(getLatest(s))) {
                 s.add(i);
                 i++;
             } else {
                 int t = s.get(s.size() - 1);
                 s.remove(s.size() - 1);
-                sum = Math.max(sum, height.get(t) * (s.isEmpty() ? i : i - s.get(s.size() - 1) - 1));
+                sum = Math.max(sum, height.get(t) * (s.isEmpty() ? i : i - getLatest(s) - 1));
             }
         }
         return sum;
+    }
+
+    private Integer getLatest(List<Integer> s) {
+        return s.get(s.size() - 1);
     }
 }
